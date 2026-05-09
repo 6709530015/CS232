a5db9db4a034eec5051f70a8196fce75e7e138d7
Karin Supareang <karin.sup@dome.tu.ac.th>
2026-05-09 11:55:22 +0700

    fix: change from async db to just non-async db.

--- app/services/sns.py
+++ app/services/sns.py
@@ -13,6 +13,7 @@ def subscribe_user_to_sns(email: str):
     region = os.getenv("AWS_REGION", "us-east-1")
     access_key = os.getenv("AWS_ACCESS_KEY_ID")
     secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+    # session_token = os.getenv("AWS_SESSION_TOKEN") | For My testing. yall can leave this part alone
 
     if not all([topic_arn, access_key, secret_key]):
         logger.warning("AWS SNS configuration is missing. Skipping subscription.")
@@ -23,7 +24,8 @@ def subscribe_user_to_sns(email: str):
             "sns",
             region_name=region,
             aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key
+            aws_secret_access_key=secret_key,
+            # aws_session_token=session_token | For My testing. yall can leave this part alone
         )
         
         response = sns_client.subscribe(
